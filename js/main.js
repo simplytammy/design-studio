c41db62a0376ce180444c999bb6962bd068c78b1
simplytammy <ogurinkabenjamin@gmail.com>
2019-05-03 16:06:41 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ document.addEventListener("click", function(e) {
 	if (e.target.closest("#sideNav") || e.target.closest("#sidebar-toggle")) return;
 	close.classList.remove("nav-active");
 });
+
+function openPage(tabName,elmnt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(tabName).style.display = "block";
+  elmnt.style.backgroundColor = "#F28C32";
+}
+document.getElementById("defaultOpen").click();
